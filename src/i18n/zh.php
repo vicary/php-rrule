e04aed1fb9256f7cad4012e35bfc368b7d290265
Vicary Archangel <vicary.archangel@member.mensa.org>
2018-06-06 17:50:22 +0800

    Linting.

--- src/i18n/zh.php
+++ src/i18n/zh.php
@@ -16,41 +16,41 @@
  */
 return array(
 	'yearly' => array(
-		'1' => '每年',
-		'else' => '每 %{interval} 年'
+		1 => '每年',
+		'else' => '每 %{interval} 年',
 	),
 	'monthly' => array(
-		'1' => '每月',
-		'else' => '每 %{interval} 月'
+		1 => '每月',
+		'else' => '每 %{interval} 月',
 	),
 	'weekly' => array(
-		'1' => '每周',
-		'2' => '每 2 周',
-		'else' => '每 %{interval} 周'
+		1 => '每周',
+		2 => '每 2 周',
+		'else' => '每 %{interval} 周',
 	),
 	'daily' => array(
-		'1' => '每日',
-		'2' => '每 2 日',
-		'else' => '每 %{interval} 日'
+		1 => '每日',
+		2 => '每 2 日',
+		'else' => '每 %{interval} 日',
 	),
 	'hourly' => array(
-		'1' => '每小時',
-		'else' => '每 %{interval} 小時'
+		1 => '每小時',
+		'else' => '每 %{interval} 小時',
 	),
 	'minutely' => array(
-		'1' => '每分鐘',
-		'else' => '每 %{interval} 分鐘'
+		1 => '每分鐘',
+		'else' => '每 %{interval} 分鐘',
 	),
 	'secondly' => array(
-		'1' => '每秒',
-		'else' => '每 %{interval} 秒'
+		1 => '每秒',
+		'else' => '每 %{interval} 秒',
 	),
 	'dtstart' => '，於 %{date} 開始',
 	'infinite' => '，直到永遠',
 	'until' => '，直到 %{date}',
 	'count' => array(
-		'1' => '，一次',
-		'else' => '，%{count} 次'
+		1 => '，一次',
+		'else' => '，%{count} 次',
 	),
 	'and' => '和',
 	'x_of_the_y' => array(
@@ -83,85 +83,85 @@ return array(
 		7 => '周日',
 	),
 	'nth_weekday' => array(
-		'1' => '第 1 個%{weekday}', // e.g. the first Monday
-		'2' => '第 2 個%{weekday}',
-		'3' => '第 3 個%{weekday}',
-		'else' => '第 %{n} 個%{weekday}'
+		1 => '第 1 個%{weekday}', // e.g. the first Monday
+		2 => '第 2 個%{weekday}',
+		3 => '第 3 個%{weekday}',
+		'else' => '第 %{n} 個%{weekday}',
 	),
 	'-nth_weekday' => array(
-		'-1' => '最後的%{weekday}', // e.g. the last Monday
-		'-2' => '倒數第 2 個%{weekday}',
-		'-3' => '倒數第 3 個%{weekday}',
-		'else' => '倒數第 %{n} 個%{weekday}'
+		-1 => '最後的%{weekday}', // e.g. the last Monday
+		-2 => '倒數第 2 個%{weekday}',
+		-3 => '倒數第 3 個%{weekday}',
+		'else' => '倒數第 %{n} 個%{weekday}',
 	),
 	'byweekno' => array(
-		'1' => '第 %{weeks} 周',
-		'else' => '第 %{weeks} 周'
+		1 => '第 %{weeks} 周',
+		'else' => '第 %{weeks} 周',
 	),
 	'nth_weekno' => '%{n}',
 	'bymonthday' => '於%{monthdays}',
 	'nth_monthday' => array(
-		'1' => '1 日',
-		'2' => '2 日',
-		'3' => '3 日',
-		'21' => '21 日',
-		'22' => '22 日',
-		'23' => '23 日',
-		'31' => '31 日',
-		'else' => '%{n} 日'
+		1 => '1 日',
+		2 => '2 日',
+		3 => '3 日',
+		21 => '21 日',
+		22 => '22 日',
+		23 => '23 日',
+		31 => '31 日',
+		'else' => '%{n} 日',
 	),
 	'-nth_monthday' => array(
-		'-1' => '最後 1 日',
-		'-2' => '倒數第 2 日',
-		'-3' => '倒數第 3 日',
-		'-21' => '倒數第 21 日',
-		'-22' => '倒數第 22 日',
-		'-23' => '倒數第 23 日',
-		'-31' => '倒數第 31 日',
-		'else' => '倒數第 ${n} 日'
+		-1 => '最後 1 日',
+		-2 => '倒數第 2 日',
+		-3 => '倒數第 3 日',
+		-21 => '倒數第 21 日',
+		-22 => '倒數第 22 日',
+		-23 => '倒數第 23 日',
+		-31 => '倒數第 31 日',
+		'else' => '倒數第 ${n} 日',
 	),
 	'byyearday' => array(
-		'1' => '年中 %{yeardays} 日',
-		'else' => '年中 %{yeardays} 日'
+		1 => '年中 %{yeardays} 日',
+		'else' => '年中 %{yeardays} 日',
 	),
 	'nth_yearday' => array(
-		'1' => '第 1',
-		'2' => '第 2',
-		'3' => '第 3',
-		'else' => '第 %{n}'
+		1 => '第 1',
+		2 => '第 2',
+		3 => '第 3',
+		'else' => '第 %{n}',
 	),
 	'-nth_yearday' => array(
-		'-1' => '最後 1',
-		'-2' => '倒數第 2',
-		'-3' => '倒數第 3',
-		'else' => '倒數第 %{n}'
+		-1 => '最後 1',
+		-2 => '倒數第 2',
+		-3 => '倒數第 3',
+		'else' => '倒數第 %{n}',
 	),
 	'byhour' => array(
-		'1' => '%{hours}',
-		'else' => '%{hours}'
+		1 => '%{hours}',
+		'else' => '%{hours}',
 	),
 	'nth_hour' => '%{n}時',
 	'byminute' => array(
-		'1' => '%{minutes}',
-		'else' => '%{minutes}'
+		1 => '%{minutes}',
+		'else' => '%{minutes}',
 	),
 	'nth_minute' => '%{n}分',
 	'bysecond' => array(
-		'1' => '%{seconds}',
-		'else' => '%{seconds}'
+		1 => '%{seconds}',
+		'else' => '%{seconds}',
 	),
 	'nth_second' => '%{n}秒',
 	'bysetpos' => '，在以上情況的第 %{setpos} 次發生',
 	'nth_setpos' => array(
-		'1' => '1',
-		'2' => '2',
-		'3' => '3',
-		'else' => '%{n}'
+		1 => '1',
+		2 => '2',
+		3 => '3',
+		'else' => '%{n}',
 	),
 	'-nth_setpos' => array(
-		'-1' => '倒數第 1',
-		'-2' => '倒數第 2',
-		'-3' => '倒數第 3',
-		'else' => '倒數第 %{n}'
+		-1 => '倒數第 1',
+		-2 => '倒數第 2',
+		-3 => '倒數第 3',
+		'else' => '倒數第 %{n}',
 	)
 );
